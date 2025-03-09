711ccbd4879b14549c2bd57ec234ab758eda8cb6
sdev-sn <sdev4422n@gmail.com>
2025-03-09 10:55:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,2 @@
-echo "# LLM_chat" >> README.md
-git init
-git add README.md
-git commit -m "Initial commit of Streamlit app"
-git branch -M main
-git remote add origin https://github.com/sdev-sn/LLM_chat.git
-git push -u origin main
-
-
 import streamlit as st
 st.write('Hello world!')
