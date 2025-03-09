795ef44adf9e7c9bd81bbf64992906f449b1195f
sdev-sn <sdev4422n@gmail.com>
2025-03-09 10:25:32 -0400

    Create aap.py

---
+++ aap.py
@@ -0,0 +1,7 @@
+echo "# LLM_chat" >> README.md
+git init
+git add README.md
+git commit -m "Initial commit of Streamlit app"
+git branch -M main
+git remote add origin https://github.com/sdev-sn/LLM_chat.git
+git push -u origin main
