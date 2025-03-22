a23aa4c5a9371b1ba7797e0444145f92cdc34022
sdev-sn <sdev4422n@gmail.com>
2025-03-22 13:29:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -66,15 +66,18 @@ db = FAISS.from_documents(documents=texts, embedding=embeddings)
 retriever = db.as_retriever()
 
 # Create QA chain
-qa = RetrievalQA.from_chain_type(llm=OpenAI(api_key=st.secrets["OPENAI_API_KEY"]), model_name="gpt-4o-mini",temperature=0,chain_type='stuff', retriever=retriever)
- 
+
+def generate_response(query_text):
+  qa = RetrievalQA.from_chain_type(llm=OpenAI(api_key=st.secrets["OPENAI_API_KEY"]), model_name="gpt-4o-mini",temperature=0,chain_type='stuff', retriever=retriever)
+  return qa.run(query_text)
+  
 st.title("ChatGPT-like clone")
 # Setup the input textfield to take questions from user
 #query_text = st.text_input('Question ', placeholder='Please provide your question here.')
-user_query = st.text_input("Ask a question about your Excel data:")
+query_text = st.text_input("Ask a question :")
 
 if user_query:
-    response = qa.run(user_query)
+    response = generate_response(query_text)
     st.write(response)
 
 # Set OpenAI API key from Streamlit secrets
