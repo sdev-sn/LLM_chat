7f8f07cb00bc541da9743872ae687fe3547a3af7
sdev-sn <sdev4422n@gmail.com>
2025-03-22 12:16:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,15 @@ import openpyxl
 #import ragatouille
 
 from openai import OpenAI
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
-from langchain.schema import HumanMessage, AIMessage
+from langchain_openai import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+
+#from langchain_openai import ChatOpenAI
+#from langchain_anthropic import ChatAnthropic
+#from langchain.schema import HumanMessage, AIMessage
 #from ragatouille import RAGPretrainedModel
 
 #RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
@@ -29,42 +35,68 @@ dotenv.load_dotenv()
 df = pd.read_excel("reddit_data_comments_feb22.xlsx")
 st.dataframe(df.head())
 #st.write("success")
+# Create a list of documents from your dataframe
+documents = []
+for index, row in df.iterrows():
+# Assuming each row is a document
+  document_text = row.to_string()
+  documents.append(document_text)
+
+# Split documents into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=2500, chunk_overlap=250)
+texts = text_splitter.create_documents(documents)
+
+# Select embeddings
+embeddings = OpenAIEmbeddings()
+
+# Create a vector store from documents
+db = Chroma.from_documents(texts, embeddings)
+
+# Create retriever interface
+retriever = db.as_retriever()
+
+# Create QA chain
+qa = RetrievalQA.from_chain_type(llm=OpenAI(api_key=st.secrets["OPENAI_API_KEY"]), chain_type='stuff', retriever=retriever)
+return qa.run(query_text)
 
 st.title("ChatGPT-like clone")
+# Setup the input textfield to take questions from user
+query_text = st.text_input('Question ', placeholder='Please provide your question here.')
+
 
 # Set OpenAI API key from Streamlit secrets
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+##client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4o-mini"
+##if "openai_model" not in st.session_state:
+##    st.session_state["openai_model"] = "gpt-4o-mini"
 
 # Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+##if "messages" not in st.session_state:
+##    st.session_state.messages = []
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+##for message in st.session_state.messages:
+##    with st.chat_message(message["role"]):
+##        st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("What is up?"):
+##if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+##    st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
+##    with st.chat_message("user"):
+ ##       st.markdown(prompt)
 
 # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
+ ##   with st.chat_message("assistant"):
+##        stream = client.chat.completions.create(
+ ##           model=st.session_state["openai_model"],
+ ##           messages=[
+ ##               {"role": m["role"], "content": m["content"]}
+  ##              for m in st.session_state.messages
             ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+  ##          stream=True,
+   ##     )
+  ##      response = st.write_stream(stream)
+##    st.session_state.messages.append({"role": "assistant", "content": response})
