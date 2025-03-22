bbef7f24901fac6c22c043fb65608555ad3d9ba7
sdev-sn <sdev4422n@gmail.com>
2025-03-22 12:46:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import dotenv
 import uuid
 import pandas as pd
 import openpyxl
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 #import ragatouille
 
 from openai import OpenAI
