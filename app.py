edaf19879f42eeac960b318baa31a404198b4a19
sdev-sn <sdev4422n@gmail.com>
2025-03-22 10:16:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import pandas as pd
 import openpyxl
 #import ragatouille
 
+from openai import OpenAI
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain.schema import HumanMessage, AIMessage
@@ -28,3 +29,26 @@ dotenv.load_dotenv()
 df = pd.read_excel("reddit_data_comments_feb22.xlsx")
 st.dataframe(df.head())
 #st.write("success")
+
+st.title("ChatGPT-like clone")
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o-mini"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
