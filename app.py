d6ccd276f4a0ac28d0aa0e7668fe815be0261aee
sdev-sn <sdev4422n@gmail.com>
2025-03-22 10:03:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,21 @@ import dotenv
 import uuid
 import pandas as pd
 import openpyxl
-import ragatouille
+#import ragatouille
 
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain.schema import HumanMessage, AIMessage
-from ragatouille import RAGPretrainedModel
+#from ragatouille import RAGPretrainedModel
 
-RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
+#RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 
-from rag_methods import (
-    load_doc_to_db, 
-    load_url_to_db,
-    stream_llm_response,
-    stream_llm_rag_response,
-)
+#from rag_methods import (
+#    load_doc_to_db, 
+#    load_url_to_db,
+#    stream_llm_response,
+#    stream_llm_rag_response,
+#)
 
 dotenv.load_dotenv()
 
