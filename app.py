85499f1605330a97f4af5bd2b3ad511a90bcb23b
sdev-sn <sdev4422n@gmail.com>
2025-04-26 16:40:01 -0400

    Update app.py
    Updated for CLaude

--- app.py
+++ app.py
@@ -11,97 +11,114 @@ import dotenv
 import uuid
 import pandas as pd
 import openpyxl
-
-#import ragatouille
-
-from openai import OpenAI
-from langchain_openai import OpenAI
-from langchain_openai import ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from chromadb.config import Settings
-from chromadb import Client
 import tiktoken
-from langchain_community.vectorstores import Chroma
+import time
+
+from chromadb import Client
 from langchain.vectorstores.faiss import FAISS
-from langchain.chains import RetrievalQA
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_core.messages import SystemMessage
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain_anthropic import ChatAnthropic
+from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
+from langchain.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import (ChatPromptTemplate, MessagesPlaceholder,
+                                    SystemMessagePromptTemplate,
+                                    HumanMessagePromptTemplate)
+
+
 
 dotenv.load_dotenv()
-
-df = pd.read_excel("reddit_data2.xlsx")
-df_data = df[['title','subreddit', 'selftext']]
-st.dataframe(df_data.head())
-
-# Convert relevant columns to string type
-for col in ['title','subreddit', 'selftext']:
-    if col in df_data.columns:
-        df_data[col] = df_data[col].astype(str)
-
-#st.write("success")
-# Create a list of documents from your dataframe
-documents = []
-for i, row in df_data.iterrows():
-# Assuming each row is a document
-  document_text = row [2]
-  documents.append(document_text)
-# return documents
-
-# Split documents into chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=250)
-texts = text_splitter.create_documents(documents)
-st.write(texts)
-
-# Select embeddings
-embeddings = OpenAIEmbeddings()
-
-# Set directory for persistent storage
-persist_directory = "./chroma_db"
-# Store documents in ChromaDB
-vectorstore = Chroma.from_documents(
-    documents=texts, 
-    embedding=embeddings,
-    persist_directory=persist_directory
-)
-
-# Create retriever interface
-##retriever = db.as_retriever()
-##retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k":5})
-retriever = vectorstore.as_retriever()
-
 ##Model
-OPENAI_API_KEY=st.secrets["OPENAI_API_KEY"]
-prompt = ChatPromptTemplate.from_template(
-    """Answer the following question based only on the provided context:
+claude_api_key =st.secrets["CLAUDE_API_KEY"]
+llm = ChatAnthropic(model="claude-3-7-sonnet-20250219", temperature=0, anthropic_api_key=claude_api_key)
 
-<context>
-{context}
-</context>
+@st.cache_resource(ttl="2h")
 
-Question: {input}"""
-)
+def pre_req(file_path):
+  template_message =[SystemMessage(content="You are helpful assistant"),
+                     MessagesPlaceholder(variable_name="chat_history"),
+                     HumanMessagePromptTemplate.from_template("{question}")]
+  prompt_template = ChatPromptTemplate.from_messages(template_message)
+  #get file
+  df = pd.read_excel(file_path)
+  df_data = df[['title','subreddit', 'selftext']]
+# st.dataframe(df_data.head())
 
+  # Convert relevant columns to string type
+  for col in ['title', 'subreddit', 'selftext']:
+    if col in df_data.columns:
+      df_data[col] = df_data[col].astype(str)
+  documents = []
+  for i, row in df_data.iterrows():
+    # Assuming each row is a document
+      document_text = row [2]
+      documents.append(document_text)
 
-llm=ChatOpenAI(openai_api_key=OPENAI_API_KEY,temperature=0,model_name="gpt-4o-mini")
-document_chain = create_stuff_documents_chain(llm, prompt)
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
+#  documents = [row[2] for _, row in df_data.iterrows()]
 
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=250)
+  texts = text_splitter.create_documents(documents)
+  embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+  persist_directory = "./chroma_db_m"
+  vectorstore = Chroma.from_documents(documents=texts, embedding=embeddings, persist_directory=persist_directory)
+  # retriever = vectorstore.as_retriever(k=100)
+  return vectorstore
 
-# Create QA chain
-#qa = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff", retriever=retriever,return_source_documents=True)
+file_path = "reddit_data2.xlsx"
+vectorstore = pre_req(file_path)
 
-##def generate_response(query):
-#  result = qa({"query": query})
-#  return result['result']
-#  qa = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff", retriever=retriever,return_source_documents=True)
-#  return qa.run(user_query)
-  
-st.title("ChatGPT-like clone")
-user_query = st.text_input("Ask a question :")
+#Strealit app
+st.title("Chat with Reddit")
+st.caption('Hi there! I am your co-pilot for Reddit insights')
 
-if user_query:
-    qa = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff", retriever=retriever,return_source_documents=True)
-    response = qa(user_query)
-    st.write(response)
+st.sidebar.title("Chat History")
+
+task = st.sidebar.radio("Mode", ['Chat', 'Reset'])
+
+if task == "Chat":
+  retriever = vectorstore.as_retriever(search_kwargs={"k": 200})
+  memory = ConversationBufferMemory(memory_key="chat_history", output_key="answer", return_messages=True)
+  template = """You are a helpful assistant. Use the following pieces of context to answer the question at the end.
+  If you don't know the answer, just say that you don't know, don't try to make up an answer."
+  context: {context}
+  {chat_history}
+  Human: {question}
+  Asistant:"""
+  prompt = PromptTemplate(input_variable =["context" ,"chat_history", "question"], template = template)
+
+#create custom chain
+  if llm is not None and retriever is not None:
+    chain = ConversationalRetrievalChain.from_llm(memory=memory, llm=llm, retriever=retriever, return_source_documents=True, combine_docs_chain_kwargs={'prompt' : prompt })
+  else:
+    logger.error("LLM or retriever is not initialized.")
+
+  task = "Reset"
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+txt = st.chat_input("Ask a question or ask for questions to chat on")
+
+if txt:
+    st.session_state['chat_history'].append("User: " +txt)
+    chat_user = st.chat_message("user")
+    chat_user.write(txt)
+    chat_assistant = st.chat_message ("assistant")
+    with st.status("Getting the answer...")as status:
+        tms_start = time.time()
+        pred = chain({"question": txt, "chat_history": st.session_state['chat_history']}, return_only_outputs=True)
+        answer = pred['answer']
+        chat_assistant.write(answer)
+        st.session_state['chat_history'].append("Assistant: " +answer)
+        tms_elapsed = time.time() - tms_start
+      #  status.update(label="Answer generated in %0.2f seconds." \
+      #                % (tms_elapsed), state="complete", expanded=True)
+    st.sidebar.markdown(
+        "<br />".join(st.session_state['chat_history'])+"<br /><br ?>",
+        unsafe_allow_html=True
+    )
